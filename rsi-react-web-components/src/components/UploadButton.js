b59c2ba891d8b019c200f0fc47f6824411b5ff0b
jzamss@gmail.com <jzamss@gmail.com>
2020-12-22 14:00:34 +0800

    rsi-react: upload button updates

--- rsi-react-web-components/src/components/UploadButton.js
+++ rsi-react-web-components/src/components/UploadButton.js
@@ -1,23 +1,25 @@
 import React, { useState } from "react";
-import Button from "@material-ui/core/Button"
+import Button from "@material-ui/core/Button";
 import Error from "./Error";
 import axios from "axios";
 import styles from "./UploadButton.css";
+import Loading from "./Loading";
 
 const UploadButton = ({
   onUpload,
   fileId,
-  caption="Upload",
-  color="secondary",
+  caption = "Upload",
+  color = "secondary",
   visibleWhen = true,
   multiple = false,
-  url="/filipizen/upload",
+  url = "/filipizen/attachment/upload"
 }) => {
   if (!visibleWhen) return null;
 
   const [error, setError] = useState();
   const [file, setFile] = useState("");
   const [progress, setProgess] = useState(0);
+  const [uploading, setUploading] = useState(false);
 
   const handleFileChange = (e) => {
     setProgess(0);
@@ -28,13 +30,17 @@ const UploadButton = ({
 
   const uploadFile = (file) => {
     setError(null);
+    setUploading(true);
+
     const formData = new FormData();
     formData.append("file", file);
-    formData.append("fileId", fileId);
+    formData.append("key", fileId);
     axios
       .post(url, formData, {
         onUploadProgress: (ProgressEvent) => {
-          let progress = Math.round((ProgressEvent.loaded / ProgressEvent.total) * 100);
+          let progress = Math.round(
+            (ProgressEvent.loaded / ProgressEvent.total) * 100
+          );
           setProgess(progress);
         }
       })
@@ -42,37 +48,46 @@ const UploadButton = ({
         const attachment = {
           name: res.data.name,
           key: res.data.key,
-          location: res.data.location,
-          path: res.data.path,
-        }
+          location: res.data.location
+        };
         onUpload(attachment);
+        setUploading(false);
       })
-      .catch((err) => setError(err));
+      .catch((err) => {
+        setError(err);
+        setUploading(false);
+      });
   };
 
   return (
     <div className={styles.UploadButton}>
       <div className={styles.UploadButton__menu}>
-        <input
-          className={styles.UploadButton__input}
-          id={fileId}
-          type="file"
-          onChange={handleFileChange}
-          multiple={multiple}
-        />
-        <label htmlFor={fileId}>
-          <Button variant="outlined" component="span" color={color}>
-            {caption || "Upload"}
-          </Button>
-        </label>
-        {progress != 0 && progress != 100 &&
+        {uploading ? (
+          <Loading msg="Uploading file..." />
+        ) : (
+          <React.Fragment>
+            <input
+              className={styles.UploadButton__input}
+              id={fileId}
+              type="file"
+              onChange={handleFileChange}
+              multiple={multiple}
+            />
+            <label htmlFor={fileId}>
+              <Button variant="outlined" component="span" color={color}>
+                {caption || "Upload"}
+              </Button>
+            </label>
+          </React.Fragment>
+        )}
+        {progress != 0 && progress != 100 && (
           <div
             className={styles.UploadButton__progress}
             style={{ width: progress }}
           >
             {progress}%
           </div>
-        }
+        )}
         <Error msg={error} />
       </div>
     </div>
